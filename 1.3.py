9ec048dc7d5737722f96f37cde8dcf9dee460ef0
AGT <rrpp@users.noreply.github.com>
2023-08-02 20:31:52 +0200

    Update 1.3.py

--- 1.3.py
+++ 1.3.py
@@ -3,6 +3,7 @@ from datetime import date
 import datetime
 import csv
 import json
+import os
 
 def obtener_precio_luz():
     url = "https://api.preciodelaluz.org/v1/prices/all?zone=PCB"
@@ -26,12 +27,15 @@ print("ejecucion: ", datetime.datetime.now())
 # Ejemplo de uso
 precios_luz = obtener_precio_luz()
 today = date.today()
+fecha_actual = today.strftime("%Y-%m-%d")
 
-# Nombre del archivo CSV
-nombre_csv = f"precios_luz_{today}.csv"
+# Rutas absolutas para el archivo CSV y el archivo JSON con el nombre del día actual
+ruta_absoluta_csv = f"/home/alfonso/data/precios_luz_{fecha_actual}.csv"
+ruta_absoluta_json = f"/home/alfonso/data/precios_luz_{fecha_actual}.json"
+
 
 # Abrir el archivo CSV en modo escritura
-with open(nombre_csv, mode='w', newline='') as file:
+with open(ruta_absoluta_csv, mode='w', newline='') as file:
     # Definir los nombres de las columnas
     fieldnames = ['Fecha', 'Hora', 'Precio (€/kWh)']
 
@@ -45,13 +49,11 @@ with open(nombre_csv, mode='w', newline='') as file:
     for hora, data in precios_luz.items():
         writer.writerow({'Fecha': data['fecha'], 'Hora': hora, 'Precio (€/kWh)': data['precio_kwh']})
 
-print(f"Los datos se han almacenado en el archivo {nombre_csv}", datetime.datetime.now())
+print(f"Los datos se han almacenado en el archivo {ruta_absoluta_csv}", datetime.datetime.now())
 
-# Nombre del archivo JSON
-nombre_json = f"precios_luz_{today}.json"
 
 # Escribir los datos en el archivo JSON
-with open(nombre_json, mode='w') as file:
+with open(ruta_absoluta_json, mode='w') as file:
     json.dump(precios_luz, file)
 
-print(f"Los datos se han almacenado en el archivo {nombre_json}:", datetime.datetime.now())
+print(f"Los datos se han almacenado en el archivo {ruta_absoluta_json}:", datetime.datetime.now())
